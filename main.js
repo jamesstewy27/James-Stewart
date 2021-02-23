8447c938bd9d2bcaa3031fb4974be4a2bb9939fb
jamesstewy27 <61468315+jamesstewy27@users.noreply.github.com>
2021-02-23 15:51:52 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+Under body:
+
+<script src="scripts/main.js"></script>
+
+End of HTML:
+
+<div><a href="../assignment-01-11458772/assignment-01.css/styles.css" target="styles"></a></div>
+<html src="C:\CS230\assignment-01-11458772\assignment-01.html" height="600" width="800" name="index.html"</html>
